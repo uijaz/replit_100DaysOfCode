7cd214cbdd4582ec1df2adea9a07b3cbf9d35f8f
uijaz <uijaz@users.noreply.github.com>
2024-11-11 23:36:45 +0000

    Create day1_100days.py

---
+++ day1_100days.py
@@ -0,0 +1,6 @@
+print("Name: Umar Ijaz")
+print("Date: 11/11/2024")
+print("Name: FirstName LastName")
+print("Date: 11/11/2024")
+print("""I'm signing up for Replit's 100 Days of Python Challenge to refresh and strengthen my Python skills!
+I commit to coding daily to complete an assignment each day and stay on track.""")
