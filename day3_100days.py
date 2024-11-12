0ef5895a63cba38644c1d97c2b4b8ac326cae459
uijaz <uijaz@users.noreply.github.com>
2024-11-12 01:07:21 +0000

    Create day3_100days.py

---
+++ day3_100days.py
@@ -0,0 +1,12 @@
+# Day 3 - Concat...what?
+print("=== Ultimate Wacky Recipe Maker ===")
+print("""Build an amazing dish for today's evening meal!""")
+print()
+food = input("Name a food: ")
+plant = input("Name a type of plant: ")
+cooking = input("Name a method of cooking: ")
+burned = input("What word describes burned food: ")
+item = input("name a DIY item: ")
+print()
+print("== Menu ==")
+print("Today's evening meal is", cooking, food, "with", burned, plant, "on a bed of", item)
