854817455c32bbe6e0e04d98326eed31c48d969f
uijaz <uijaz@users.noreply.github.com>
2024-11-13 10:15:37 +0000

    Create day7_100days.py

---
+++ day7_100days.py
@@ -0,0 +1,18 @@
+# Day 7 - Nesting Dolls Code
+player = input("Which football star do you like? ")
+if player == "ronaldo":
+  print("Awesome!")
+  number = input("What number shirt does he wear? ")
+  if number == "7":
+    print("You are a Ronaldo fan!")
+  else: 
+    print("No joy!")
+elif player == "salah":
+  print("Awesome!")
+  number = input("What number shirt does he wear? ")
+  if number == "11":
+    print("You are a Salah fan!")
+  else:
+    print("No joy!")
+else:
+  print("Are you into football?")
