e91bb755bc92ac0e703e522294e13ff898bbf82d
uijaz <uijaz@users.noreply.github.com>
2024-11-13 23:15:34 +0000

    Create day10_100days.py

---
+++ day10_100days.py
@@ -0,0 +1,8 @@
+# Day 10 - Show Me the $$
+myBill = float(input("What was the bill?: "))
+tip = int(input("What percentage of tip would you like to leave? "))
+totalBill = myBill + (myBill * (tip / 100))
+numberOfPeople = int(input("How many people?: "))
+answer = totalBill / numberOfPeople
+answerTwoDec = round(answer, 2)
+print("You all owe", answerTwoDec)
