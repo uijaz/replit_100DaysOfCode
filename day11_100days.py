1f9b2a0a2fa2dc05e560a12c9cc752e1117f1d14
uijaz <uijaz@users.noreply.github.com>
2024-11-14 07:15:00 +0000

    Create day11_100days.py

---
+++ day11_100days.py
@@ -0,0 +1,14 @@
+# Day 11 - "525,600 minutes"
+print("== How many seconds are in a year? ==")
+yearType = input("Is it a leap year? (y/n) ")
+
+if yearType == "y":
+  # 31622400 seconds
+  seconds = 60 * 60 * 24 * 366
+  print("There are", seconds, "seconds in a leap year.")
+elif yearType == "n":
+  # 31536000 seconds
+  seconds = 60 * 60 * 24 * 365
+  print("There are", seconds, "seconds in a year.")
+else:
+  print("Invalid input.")
