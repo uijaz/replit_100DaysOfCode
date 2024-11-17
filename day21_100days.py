57ed53609dea6e5baf5cf49d09d6bcd45344df24
uijaz <uijaz@users.noreply.github.com>
2024-11-17 07:49:19 +0000

    Create day21_100days.py

---
+++ day21_100days.py
@@ -0,0 +1,21 @@
+# Day 21 - Throwback to Math Facts
+print("== Math Game! ==")
+
+while True:
+  multiplicand = int(input("Name your multiplicand: "))
+  print()
+  score = 0
+  for i in range(1, 11):
+    answer = int(input(f"{i} x {multiplicand} = "))
+    if answer == i * multiplicand:
+      print("Great work!")
+      score += 1
+    else:
+      print("No joy! The answer was", i * multiplicand)
+  print()
+  print(f"You scored {score} out of {i}.")
+  tryAgain = input("Would you like to try again (y / n)? ")
+  print("---")
+  if tryAgain != "y" and tryAgain != "Y":
+    break
+print("Keep practicing!")
