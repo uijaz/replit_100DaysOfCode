ce4cbb7e1455f98b5a428c400b5211f9c4e368db
uijaz <uijaz@users.noreply.github.com>
2024-11-19 15:45:11 +0000

    Create day23_100days.py

---
+++ day23_100days.py
@@ -0,0 +1,23 @@
+# Day 23 - Subroutines: The Recipe for Coding
+def login():
+  print("Replit Login System")
+  print()
+  uname = input("What is your username?: ")
+  pword = input("What is your password? ")
+  
+  if uname == name and pword == word:
+    print("Welcome to Replit!")
+    return 1
+  else:
+    print("Whoops! Username or password do not match. Try again!")
+    return 0
+
+name = "david"
+word = "pass"
+
+while True:
+  print()
+  if login():
+    break
+  else:
+    continue
